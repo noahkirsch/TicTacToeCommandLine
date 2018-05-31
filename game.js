b00c19f14cca76dd271470b96d5e30943377d96a
Noah Kirschbaum <noah.kirschbaum@gmail.com>
2018-05-31 12:06:03 -0700

    Add checkWinner and makeMove functions

--- game.js
+++ game.js
@@ -1,3 +1,4 @@
+ var prompt = require('prompt');
 
 var board = [' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' '];
 var xMove = true;
@@ -21,6 +22,7 @@ var printBoard = function() {
 }
 
 var makeMove = function() {
+
 	if (checkWinner) {
 		if (xMove) {
 			console.log("X Wins!");
@@ -29,15 +31,57 @@ var makeMove = function() {
 		}
 	}
 
-	printBoard();
 }
 
 var checkWinner = function() {
+	if (board[0] === board[1] === board[2]) {
+		return true;
+	} else if (board[3] === board[4] === board[5]) {
+		return true;
+	} else if (board[6] === board[7] === board[8]) {
+		return true;
+	} else if (board[0] === board[3] === board[6]) {
+		return true;
+	} else if (board[1] === board[4] === board[7]) {
+		return true;
+	} else if (board[2] === board[5] === board[8]) {
+		return true;
+	} else if (board[0] === board[4] === board[8]) {
+		return true;
+	} else if (board[2] === board[4] === board[6]) {
+		return true;
+	}
 
+	return false;
 }
 
+var makeMove = function() {
+	prompt.start();
 
+	prompt.get(['Move'], function (err, result) {
+	  if (err) { return onErr(err); }
+	  if (xMove) {
+	  	board[result.Move - 1] = 'X';
+	  } else {
+	  	board[result.Move - 1] = 'O';
+	  }
+	  console.log(printBoard());
 
+		if (checkWinner()) {
+			if (xMove) {
+				console.log("X Wins!");
+			} else {
+				console.log("O Wins!");
+			}
+		}
 
+		xMove = !xMove;
+
+	  makeMove();
+	});
+}
 
 console.log(printBoard());
+makeMove();
+
+
